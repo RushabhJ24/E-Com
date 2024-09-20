4dbfc66968f1b5cc9d43640163cd58b13135bb11
Rushabh <rushabhsjain40@gmail.com>
2024-09-20 13:35:26 +0530

    Added

--- server.js
+++ server.js
@@ -15,6 +15,15 @@ const productRoutes = require('./routes/product');
 const braintreeRoutes = require('./routes/braintree');
 const orderRoutes = require('./routes/order');
 
+// Serve static assets if in production
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, 'client/build')));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
+
 // app
 const app = express();
 
