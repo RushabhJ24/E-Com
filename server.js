fea2fcc1b231c1cab967a99a7801050b979d7b2b
Rushabh <rushabhsjain40@gmail.com>
2024-09-22 15:22:16 +0530

    added the build

--- server.js
+++ server.js
@@ -74,7 +74,7 @@ if (process.env.NODE_ENV === 'production') {
   });
 }
 
-const PORT = process.env.PORT || 10000;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
